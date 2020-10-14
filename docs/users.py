81700efc6f7d72b45af89f82b8ef4849b38f89ed
astherath <farceriv@gmail.com>
2020-10-14 19:10:12 -0400

    single fix for docs

--- docs/users.py
+++ docs/users.py
@@ -4,6 +4,10 @@ Inserts the registration form to the user's database.
 registration_summ = """
 Register User
 """
+
 delete_user_desc = """
+Delete user from database based off of unique identifier
+"""
+delete_user_summ = """
 Delete User
 """
