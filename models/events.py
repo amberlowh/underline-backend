d95007549b04fc4533e02636b363eea20cb5fa54
astherath <farceriv@gmail.com>
2020-10-21 19:04:41 -0400

    fixed response model for get events by status call and added error handling.
    Now empty query returns 404 immediately. New response model is based off of a list of events.

--- models/events.py
+++ models/events.py
@@ -5,28 +5,34 @@ from enum import Enum
 
 import datetime
 
+
 # incomplete
+# TODO: fix this name to be single event (doesnt make sense for plural)
 class Events(BaseModel):
-  title: str
-  #description: str
-  #date: datetime
-  #tag: TagEnum
-  #location: Location
-  #max_capacity: int
-  #public: bool
-  #attending: List[models.Users]
-  #upvotes: int
-  #comment_ids: List[str]
-  #rating: float
-  #status: StatusEnum
-  #creator_id: str
-  # TODO: add landmark flag OR extend into own class
-  # TODO: think about how to handle expiration based on dates
+    title: str
+    #description: str
+    #date: datetime
+    #tag: TagEnum
+    #location: Location
+    #max_capacity: int
+    #public: bool
+    #attending: List[models.Users]
+    #upvotes: int
+    #comment_ids: List[str]
+    #rating: float
+    #status: StatusEnum
+    #creator_id: str
+    # TODO: add landmark flag OR extend into own class
+    # TODO: think about how to handle expiration based on dates
+
 
 class registration_form(Events):
-  pass
+    pass
+
 
 class registration_response(BaseModel):
-  event_id: str
+    event_id: str
 
 
+class get_all_events_by_status_response(BaseModel):
+    events: List[Events]
