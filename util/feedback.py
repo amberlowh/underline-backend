03142678e30f124c47a02bb3d0c002814a59d529
Bartolini <danielbartolini@daniels-mbp.attlocal.net>
2020-11-02 12:08:05 -0500

    added upvote to feedback endpoint

--- util/feedback.py
+++ util/feedback.py
@@ -1,11 +1,3 @@
-# utils/feedback.py
-def add_upvote_to_feedback(feedback_id, db):
-  collection = db[DB_NAME]["feedback"]
-  collection.update_one({"_id": feedback_id}, {"$inc": {"upvotes": 1}})
-
-  #ADD UPVOTE TO FEEDBACKyou dont need to do models
-
-=======
 import uuid
 from starlette.exceptions import HTTPException
 from config.db import get_database
@@ -22,14 +14,18 @@ async def delete_feedback(event_id, feedback_id, db):
     if not found_event:
         raise HTTPException(status_code=404,
                             detail="Event ID is invalid")
+                            
 
 
     if feedback_id in found_event["comment_ids"]:
         found_event["comment_ids"].remove(feedback_id)
     else:
         raise HTTPException(status_code=404,
-                        detail="Feedback ID not found in the provided event")
+                            detail="Feedback ID not found in the provided event")
                             
     column.update_one({"_id": event_id}, {"$set": found_event})
->>>>>>> 129f9504e497a3f699d2dbfa17ea518c13b72321
-  
+
+    def add_upvote_to_feedback(feedback_id, db):
+        #addupvote
+        collection = db[DB_NAME]["feedback"]
+        collection.update_one({"_id": feedback_id}, {"$inc": {"upvotes": 1}})
