b78162787f59d65abe7646e53b3b6f49fac1bf72
Bartolini <danielbartolini@daniels-mbp.attlocal.net>
2020-11-02 19:37:35 -0500

    added upvote to feedback

--- routes/feedback.py
+++ routes/feedback.py
@@ -1,2 +1,56 @@
 
+from config.db import get_database
+from fastapi import APIRouter, Response
 
+from models import events as eventModel
+
+#from docs import feedback as docs
+import logging
+import util.feedback as utils
+from starlette.exceptions import HTTPException
+
+router = APIRouter() 
+
+@router.delete(
+    "/feedback/delete/{event_id}/{feedback_id}",
+    #description=
+    #summary=
+    #tags=
+    status_code=204,
+)
+async def delete_event(event_id, feedback_id):                   
+
+    # get DB Database
+    db = get_database()
+
+    # perform deletion
+    await utils.delete_feedback(event_id, feedback_id, db)
+
+    # prevent console error
+    return Response(status_code=204) # https://github.com/tiangolo/fastapi/issues/717#issuecomment-583826657
+"""???doesnt need endpoint???
+@router.add_upvote{
+    "/feedback/???/{event_id}/{feedback_id}",
+    #description=
+    #summary=
+    #tags=
+    status_code=200,
+
+}
+"""
+async def add_upvote_to_feedback(event_id, feedback_id):                   
+
+    # get DB Database
+    db = get_database()
+
+    # increase upvote count
+    await utils.add_upvote_to_feedback(feedback_id, db)
+
+   """
+   ??
+    return Response(status_code=200) # https://github.com/tiangolo/fastapi/issues/717#issuecomment-583826657
+    """
+
+
+   
+    
