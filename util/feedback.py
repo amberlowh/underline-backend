a45b90f38ba6695fe65d916f30adb473470b5aa5
michaeltrac <michael@DESKTOP-1PJMF64.localdomain>
2020-11-03 20:28:10 -0500

    lmk

--- util/feedback.py
+++ util/feedback.py
@@ -2,28 +2,27 @@ import uuid
 from starlette.exceptions import HTTPException
 from config.db import get_database
 import logging
+
 DB_NAME = "underline"
+
 
 # Given an event id and feedback id, attempt to delete the feedback from the event's comment_ids array
 async def delete_feedback(event_id, feedback_id, db):
-    
     column = db[DB_NAME]["events"]
 
     found_event = column.find_one({"_id": event_id})
-    
+
     if not found_event:
         raise HTTPException(status_code=404,
                             detail="Event ID is invalid")
-
 
     if feedback_id in found_event["comment_ids"]:
         found_event["comment_ids"].remove(feedback_id)
     else:
         raise HTTPException(status_code=404,
                             detail="Feedback ID not found in the provided event")
-                            
-    column.update_one({"_id": event_id}, {"$set": found_event})
 
+    column.update_one({"_id": event_id}, {"$set": found_event})
 
 
 # Returns event dictionary
