795a48c09b1847bc6abf802573aa66416f02d1ad
michaeltrac <michael@DESKTOP-1PJMF64.localdomain>
2020-11-03 20:12:41 -0500

    lmk

--- routes/feedback.py
+++ routes/feedback.py
@@ -1,12 +1,11 @@
-
 from config.db import get_database
 from fastapi import APIRouter, Response
-
 from models import events as eventModel
-
 #from docs import feedback as docs
 import logging
 import util.feedback as utils
+from util import events as utilss
+
 from starlette.exceptions import HTTPException
 
 router = APIRouter()
@@ -28,5 +27,12 @@ async def delete_event(event_id, feedback_id):
 
     # prevent console error
     return Response(status_code=204) # https://github.com/tiangolo/fastapi/issues/717#issuecomment-583826657
-   
-    
+
+
+
+
+@router.get("/users/{feedback}", response_model=eventModel.Events, status_code=201)
+async def get_feedback(feedback_id):
+    db = get_database()
+    feedback_data = await utilss.get_feedback(feedback_id, db)
+    return eventModel.Events(**feedback_data)
