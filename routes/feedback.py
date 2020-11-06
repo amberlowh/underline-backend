5d92df2194ce469c0efc3a05e9f455b5ac6c3db4
k07-repo <kpuli007@fiu.edu>
2020-11-05 20:29:51 -0500

    Create feedback addition endpoint and add comment field to feedback

--- routes/feedback.py
+++ routes/feedback.py
@@ -3,7 +3,7 @@ from config.db import get_database
 from fastapi import APIRouter, Response
 
 from models import events as eventModel
-
+from models import feedback as feedbackModel
 #from docs import feedback as docs
 import logging
 import util.feedback as utils
@@ -18,7 +18,7 @@ router = APIRouter()
     #tags=
     status_code=204,
 )
-async def delete_event(event_id, feedback_id):                   
+async def delete_feedback(event_id, feedback_id):                   
 
     # get DB Database
     db = get_database()
@@ -29,4 +29,21 @@ async def delete_event(event_id, feedback_id):
     # prevent console error
     return Response(status_code=204) # https://github.com/tiangolo/fastapi/issues/717#issuecomment-583826657
    
-    
+@router.post(
+    "/feedback/add",
+    response_model=feedbackModel.registration_response,
+    #description=docs.registration_desc,
+    #summary=docs.registration_summ,
+    #tags=["Users"],
+    status_code=201,
+)
+async def add_feedback(form: feedbackModel.registration_form):
+
+    # get DB instance
+    db = get_database()
+
+    # send the form data and DB instance to util.users.register_user
+    feedback_id = await utils.add_feedback(form, db)
+
+    # return response in reponse model
+    return feedbackModel.registration_response(feedback_id=feedback_id)
