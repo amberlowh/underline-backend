f16413985240c8f61b338586dc91467c2c064f98
astherath <farceriv@gmail.com>
2020-11-09 20:05:25 -0500

    added registered event fixture to the conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import pytest
 import os
 from config.db import connect_to_mongo, close_connection_to_mongo
 from fastapi.testclient import TestClient
+from datetime import datetime
+
 from app import app
 
 client = TestClient(app)
@@ -28,18 +30,46 @@ def registered_user():
     response = client.post("/users/register", json=user_data)
     return user_data
 
+
 @pytest.fixture(scope='module')
 def registered_event():
-    event_data = {"lat":"75", "lon":"50", "radius": 20}
+    event_data = {"lat": "75", "lon": "50", "radius": 20}
     response = client.get("/events/register", json=event_data)
     return response
 
+
 @pytest.fixture(scope='module')
 def events_locations():
-    location_data  = {"lat":"75", "lon":"50", "radius": 20}
+    location_data = {"lat": "75", "lon": "50", "radius": 20}
     response = client.get("/events/location/", params=location_data)
     return response
+
 
 @pytest.fixture(autouse=True)
 def run_around_tests():
     yield
+
+
+@pytest.fixture(scope='module')
+def registered_event_data():
+    json = {
+        "title": "Test Event",
+        "description": "Test event",
+        "date": str(datetime.now()),
+        "tag": "sporting_events",
+        "location": {
+            "latitude": 75.0,
+            "longitude": 75.0
+        },
+        "max_capacity": 10,
+        "public": False,
+        "attending": [],
+        "upvotes": 0,
+        "comment_ids": [],
+        "rating": 5.0,
+        "status": "active",
+        "creator_id": "0"
+    }
+    response = client.post("/events/register", json=json)
+    assert response.status_code == 201
+    return json
